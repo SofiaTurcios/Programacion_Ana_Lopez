6746703574fc320c06a96d76fdec8a7efb2aee94
SofiaTurcios <136513214+SofiaTurcios@users.noreply.github.com>
2023-07-04 18:05:03 -0600

    Ejercicio.cpp

--- 0407.cpp
+++ 0407.cpp
@@ -1,52 +1,58 @@
 #include <iostream>
-#include <string>
+#include <string.h>
 
 using namespace std;
 
-string nombres[10];
-string apellidos[10];
-int cursos;
-int notas;
+string nombres;
+int cursos=0;
+int notas[100];
+float promedio=0.0;
 int i;
 
 int main() {
 	while (i != 5){
  cout<<"Elija una opcion"<<endl;
- cout<<"ingresar nombres y apellidos"<<endl;
- cout<<"ingresar las notas de los cursos"<<endl;
- cout<<"visualizar las notas"<<endl;
- cout<<"promedio"<<endl;
- cout<<"salir"<<endl;
+ cout<<"1. ingresar nombres y apellidos"<<endl;
+ cout<<"2. ingresar las notas de los cursos"<<endl;
+ cout<<"3. visualizar las notas"<<endl;
+ cout<<"4. promedio"<<endl;
+ cout<<"5. salir"<<endl;
+ 
  cin>>i;
-
 	
 	switch(i){
 		case 1: cout<<"ingrese el nombre y apellido"<<endl;
-		for(int i=0; i<10; i++){
-			cin>>nombres[i];
-			cin>>apellidos[i];
-	}
+	    cin.ignore();
+	    getline(cin, nombres);
+	    
+		cout<<" "<<endl;
 	break;
 	
-	case 2: cout<<"ingrese las notas de los cursos"<<endl;
-    	cout<<"ingrese la cantidad de cursos"<<endl;
+	
+	case 2: cout<<"ingrese la cantidad de cursos"<<endl;
     	cin>>cursos;
-		for(int i=0; i<10; i++){
+		for(int i=0; i<cursos; i++){
 			cout<< "curso"<<i+1<<endl;
-			cin>>notas;
-	}
+			cin>>notas[i];
+	    }	
+	    cout<<" "<<endl;
 	break;
+	
 	
 	case 3: cout<<"las notas son"<<endl;
-		for(int i=0; i<10; i++){
-	    	cout<<"curso "<<i+1<<notas<<endl;
-	}
+		for(int i=0; i<cursos; i++){
+	    	cout<<"curso "<<i+1<<": "<<notas[i]<<endl;
+	   }
+	   cout<<" "<<endl;
 	break;
 	
+	
     case 4: cout<<"el promedio general es: "<<endl;
-		for(int i=0; i<10; i++){
-			cin>>notas;
+		for(int i=0; i<cursos; i++){
+			promedio+=notas[i];
 	}
+	promedio/=cursos;
+	cout<<fixed<<promedio<<endl;
 	break;
 	
 	return 0;
